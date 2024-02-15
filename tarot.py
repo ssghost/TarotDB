93028deefcee3c09eb9b9e868830cde798b4e7d6
ssghost <ssprof0@gmail.com>
2024-02-15 23:50:15 +0800

    mod init

--- tarot.py
+++ tarot.py
@@ -4,17 +4,17 @@ import random
 from typing import List
 
 class Tarot:
-    def __init__() -> None:
-        pass
+    async def __init__(self) -> None:
+        async with Surreal("ws://localhost:8000/rpc") as db:
+            await db.signin({"user": "root", "pass": "root"})
+            await db.use("tarotdb", "tarotdb")
+            self.db = db
 
     async def upload(self) -> None:
         with open("./deck.json") as f:
             df = f.read()
-        async with Surreal("ws://localhost:8000/rpc") as db:
-            await db.signin({"user": "root", "pass": "root"})
-            await db.use("tarotdb", "tarotdb")
-            await db.delete(df)
-            await db.create(df)
+            await self.db.delete(df)
+            await self.db.create(df)
     
     async def pickcard(self, n:int, scope:str='all') -> List[str]:
         if scope == 'all':
@@ -25,28 +25,19 @@ class Tarot:
             rlist = list(range(22,78))
         pick: List[int] = random.sample(rlist, n)
         reslist: List[str] = []
-        async with Surreal("ws://localhost:8000/rpc") as db:
-            await db.signin({"user": "root", "pass": "root"})
-            await db.use("tarotdb", "tarotdb")
-            for p in pick:
-                res = await db.query("SELECT * FROM deck WHERE enum=type::number($enum)", {'enum':p})
-                reslist.append(str(res))
-                await db.query("UPDATE deck SET picked=picked+1 WHERE enum=type::number($enum)", {'enum':p})
+        for p in pick:
+            res = await self.db.query("SELECT * FROM deck WHERE enum=type::number($enum)", {'enum':p})
+            reslist.append(str(res))
+            await self.db.query("UPDATE deck SET picked=picked+1 WHERE enum=type::number($enum)", {'enum':p})
         return reslist
     
     async def viewcard(self, name:str) -> str:
-        async with Surreal("ws://localhost:8000/rpc") as db:
-            await db.signin({"user": "root", "pass": "root"})
-            await db.use("tarotdb", "tarotdb")
-            res = await db.query("SELECT * FROM deck WHERE name=type::string($name)", {'name':name})
+        res = await self.db.query("SELECT * FROM deck WHERE name=type::string($name)", {'name':name})
         return str(res)
     
     async def changehost(self) -> str:
         pick: int = random.randint(0,21)
-        async with Surreal("ws://localhost:8000/rpc") as db:
-            await db.signin({"user": "root", "pass": "root"})
-            await db.use("tarotdb", "tarotdb")
-            await db.query("UPDATE deck SET is_host=false")
-            res = await db.query("UPDATE deck SET is_host=true WHERE enum=type::number($enum)", {'enum':pick})
+        await self.db.query("UPDATE deck SET is_host=false")
+        res = await self.db.query("UPDATE deck SET is_host=true WHERE enum=type::number($enum)", {'enum':pick})
         return str(res)
     
