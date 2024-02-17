7b20f8b1132431723527e21eb808213675260821
ssghost <ssprof0@gmail.com>
2024-02-17 12:09:01 +0800

    fin nodb

--- tarot_nodb.py
+++ tarot_nodb.py
@@ -11,8 +11,7 @@ class TarotNodb:
         with open("./deck.json", 'w') as f:
             self.deck.to_json(f)
             
-    
-    async def pickcard(self, n:int, scope:str='all') -> List[str]:
+    def pickcard(self, n:int, scope:str='all') -> List[str]:
         if scope == 'all':
             rlist = list(range(78))
         elif scope == 'major':
@@ -22,20 +21,19 @@ class TarotNodb:
         pick: List[int] = random.sample(rlist, n)
         reslist: List[str] = []
         for p in pick:
-            res = await self.db.query("SELECT * FROM deck WHERE enum=type::number($enum)", {'enum':p})
+            res = self.deck.loc[self.deck['enum']==p]
             reslist.append(str(res))
-            await self.db.query("UPDATE deck SET picked=picked+1 WHERE enum=type::number($enum)", {'enum':p})
+            self.deck.loc[self.deck['enum']==p]['picked']+=1
         return reslist
     
-    async def viewcard(self, name:str) -> str:
-        res = await self.db.query("SELECT * FROM deck WHERE name=type::string($name)", {'name':name})
+    def viewcard(self, name:str) -> str:
+        res = self.deck.loc[self.deck['name'] == name]
         return str(res)
     
-    async def changehost(self) -> str:
+    def changehost(self) -> str:
         pick: int = random.randint(0,21)
-        await self.db.query("UPDATE deck SET is_host=false")
-        res = await self.db.query("UPDATE deck SET is_host=true WHERE enum=type::number($enum)", {'enum':pick})
+        self.deck['is_host'] = False
+        self.deck.loc[self.deck['enum']==pick]['is_host'] = True
+        res = self.deck.loc[self.deck['enum']==pick] 
         return str(res)
     
-    async def close(self) -> None:
-        await self.db.close()
