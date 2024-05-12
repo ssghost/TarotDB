d50cd77c3b94193ac47a2ca4d708e7b4836c15b6
ssghost <ssprof0@gmail.com>
2024-05-12 12:44:31 +0800

    weekly checkin

---
+++ choice.py
@@ -0,0 +1,5 @@
+from tarot_nodb import *
+
+tndb = TarotNodb()
+print(tndb.pickcard(5))
+tndb.writejson()
---
+++ weekly.py
@@ -0,0 +1,5 @@
+from tarot_nodb import *
+
+tndb = TarotNodb()
+print(tndb.pickcard(2))
+tndb.writejson()
